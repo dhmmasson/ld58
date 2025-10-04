46c3a55dad75ac3f22d744799c54775aa01435a1
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2025-10-04 10:37:08 +0200

    refactor(splash): move splash to its own file

---
+++ src/SplashHandler.js
@@ -0,0 +1,37 @@
+const SplashHandler = {
+  opacity: 0,
+  enter: function () {
+    this.opacity = 255;
+    console.log("Enter Splash");
+  },
+  draw: function () {
+    this.opacity = lerp(this.opacity, 0, 0.01);
+    background(0);
+    fill(255);
+    noStroke();
+    textSize(100);
+
+    fill("#ee5533");
+    textAlign(RIGHT, CENTER);
+    text("LD", width / 2, height / 2);
+    fill("#f79122");
+    textAlign(LEFT, CENTER);
+    text("58", width / 2, height / 2);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("The Game", width / 2, height / 2 + 100);
+    textSize(20);
+    text("Made in 48 hours", width / 2, height / 2 + 160);
+    text("by Dimitri Masson", width / 2, height / 2 + 180);
+    text("Click to start", width / 2, height / 2 + 200);
+
+    fill(0, this.opacity);
+    rect(0, 0, width, height);
+  },
+  mousePressed: function () {
+    console.log("Splash mousePressed");
+    Game.changeMode(GameState.MENU);
+  },
+};
+Game.handlers.splash = SplashHandler;
+Game.changeMode(GameState.SPLASH);
--- src/game.js
+++ src/game.js
@@ -24,7 +24,7 @@ const Game = {
 };
 
 function setup() {
-  console.clear();
+  // console.clear();
   const canvasElement = document.getElementById("canvasContainer");
   console.log(canvasElement);
   // create a biiiiig canvas to start with
@@ -48,43 +48,6 @@ function mousePressed() {
     Game.currentHandler.mousePressed();
   }
 }
-
-const SplashHandler = {
-  opacity: 0,
-  enter: function () {
-    this.opacity = 255;
-    console.log("Enter Splash");
-  },
-  draw: function () {
-    this.opacity = lerp(this.opacity, 0, 0.01);
-    background(0);
-    fill(255);
-    noStroke();
-    textSize(100);
-
-    fill("#ee5533");
-    textAlign(RIGHT, CENTER);
-    text("LD", width / 2, height / 2);
-    fill("#f79122");
-    textAlign(LEFT, CENTER);
-    text("58", width / 2, height / 2);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    text("The Game", width / 2, height / 2 + 100);
-    textSize(20);
-    text("Made in 48 hours", width / 2, height / 2 + 160);
-    text("by Dimitri Masson", width / 2, height / 2 + 180);
-    text("Click to start", width / 2, height / 2 + 200);
-
-    fill(0, this.opacity);
-    rect(0, 0, width, height);
-  },
-  mousePressed: function () {
-    console.log("Splash mousePressed");
-    Game.changeMode(GameState.MENU);
-  },
-};
-Game.handlers.splash = SplashHandler;
 
 const MenuHandler = {
   enter: function () {
