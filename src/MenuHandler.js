d6663bd7b4fc20feb6d98d2068cad58e828967b0
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2025-10-05 21:13:27 +0200

    fix(menu): remove p5js old menu

--- src/MenuHandler.js
+++ src/MenuHandler.js
@@ -41,18 +41,6 @@ const MenuHandler = {
   },
   draw: function () {
     background(255);
-    // Level Selection (levels from levels.js)
-    textAlign(CENTER, CENTER);
-
-    fill(0);
-    textSize(32);
-    text("Tutoriel", width / 2, 50);
-    textSize(24);
-
-    buttons.forEach((button) => button.draw());
-
-    textSize(16);
-    text("Click to Start", width / 2, height - 50);
   },
   mousePressed: function () {
     buttons.forEach((button) => button.mousePressed());
